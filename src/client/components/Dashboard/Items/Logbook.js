97fd6ec11525910c196f9808c946b37e062559ba
Peter Chim <pchim24@gmail.com>
2016-06-27 01:56:42 -0700

    Initialize log activity options

--- src/client/components/Dashboard/Items/Logbook.js
+++ src/client/components/Dashboard/Items/Logbook.js
@@ -2,10 +2,36 @@ import React, { Component } from 'react';
 
 const Logbook = () => (
   <div className="main-container">
-    <div className="top-container">
-      <div>
-        Card
+    <div className="top-container horizontal-dash debug">
+      <div className="log-activity card debug">
+        <div className="image debug">
+        </div>
+        <div className="content">
+          Card
+        </div>
       </div>
+      <div className="log-activity card debug">
+        <div className="image debug">
+        </div>
+        <div className="content">
+          Card
+        </div>
+      </div>
+      <div className="log-activity card">
+        <div className="image">
+        </div>
+        <div className="content">
+          Card
+        </div>
+      </div>
+      <div className="log-activity card">
+        <div className="image">
+        </div>
+        <div className="content">
+          Card
+        </div>
+      </div>
+
     </div>
   </div>
 );
